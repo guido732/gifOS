6e888595e2e9abc47d3d2e9c702b320d0f6b0cf2
guido732 <guido732@gmail.com>
2019-11-19 08:44:35 -0300

    Adds dropdown closing functionality on click outside

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-// TODO close on click outside
-// https://www.blustemy.io/detecting-a-click-outside-an-element-in-javascript/
+// Dropdown list visibility toggle
 document.querySelector("#dropdown-btn").onclick = function(e) {
 	e.preventDefault();
 	const $dropdownList = document.querySelector("#dropdown-list");
 	$dropdownList.style.display = $dropdownList.style.display === "block" ? "none" : "block";
+};
+
+// Closes dropdown on click outside
+document.onclick = function(e) {
+	if (e.target === document.querySelector("body")) {
+		$dropdownList.style.display = "none";
+	}
 };
