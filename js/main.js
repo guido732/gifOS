933541c04b6f452b150bef911df152b53a4ba5b7
Guido Torres <guido732@gmail.com>
2019-11-20 08:58:48 -0300

    Fixes variable scope in Js which prevented closing event to work properly

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 // Dropdown list visibility toggle
 document.querySelector("#dropdown-btn").onclick = function(e) {
-	e.preventDefault();
-	const $dropdownList = document.querySelector("#dropdown-list");
-	$dropdownList.style.display = $dropdownList.style.display === "block" ? "none" : "block";
+  const $dropdownList = document.querySelector("#dropdown-list");
+  e.preventDefault();
+  $dropdownList.style.display =
+    $dropdownList.style.display === "block" ? "none" : "block";
 };
 
 // Closes dropdown on click outside
 document.onclick = function(e) {
-	if (e.target === document.querySelector("body")) {
-		$dropdownList.style.display = "none";
-	}
+  const $dropdownList = document.querySelector("#dropdown-list");
+  if (e.target === document.querySelector("body")) {
+    $dropdownList.style.display = "none";
+  }
 };
