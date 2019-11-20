bebf2ffc95984af64af650a8098992e31be450c2
guido732 <guido732@gmail.com>
2019-11-20 09:29:29 -0300

    Removes duplicate code section left previously in merge

--- js/main.js
+++ js/main.js
@@ -1,21 +1,13 @@
 // Dropdown list visibility toggle
 document.querySelector("#dropdown-btn").onclick = function(e) {
-  const $dropdownList = document.querySelector("#dropdown-list");
-  e.preventDefault();
-  $dropdownList.style.display =
-    $dropdownList.style.display === "block" ? "none" : "block";
+	const $dropdownList = document.querySelector("#dropdown-list");
+	e.preventDefault();
+	$dropdownList.style.display = $dropdownList.style.display === "block" ? "none" : "block";
 };
 
 // Closes dropdown on click outside
 document.onclick = function(e) {
-  const $dropdownList = document.querySelector("#dropdown-list");
-  if (e.target === document.querySelector("body")) {
-    $dropdownList.style.display = "none";
-  }
-};
-
-// Closes dropdown on click outside
-document.onclick = function(e) {
+	const $dropdownList = document.querySelector("#dropdown-list");
 	if (e.target === document.querySelector("body")) {
 		$dropdownList.style.display = "none";
 	}
