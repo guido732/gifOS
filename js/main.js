4a5d6e6bf7e716514908abfef5b123ae4e922342
Guido Torres <guido732@gmail.com>
2019-12-02 22:54:02 -0300

    Feature/suggestions-trends (#7)
    * Adds header bar
    
    * Adds window element and grid to display results
    
    * Adds styles to window elements in suggestion section
    
    * Fixes centering of screen for larger screens
    
    * Adds class to make elements span 2 columns in the grid
    
    * Makes grid item-double responsive to span 1 col only in mobile and dense grid
    
    * Adds breakpoint mixins
    
    * Replaces CSS background-image for IMG HTML tags with corresponding CSS styles
    
    * First demo API fetch request
    
    * Adds trending request to giphy and displays it with proper formatting
    
    * Creates functions to clean up code and make it more legible
    
    * Function that fetches suggestion based on array of keywords
    
    * Adds functionality to automatize double span elements depending on image ratio

--- js/main.js
+++ js/main.js
@@ -22,3 +22,83 @@ document.querySelector("#search-bar").oninput = function(e) {
 		$searchButton.disabled = true;
 	}
 };
+
+// Fetch new items on page load for trending section
+window.onload = () => {
+	fetchTrending(20);
+	fetchSuggestions(4);
+};
+
+const APIkey = "KvIjm5FP077DsfgGq2kLnXDTViwRJP7f";
+function fetchTrending(limit) {
+	const $suggestedGifsContainer = document.querySelector("#trend-grid");
+	const gifOffset = Math.floor(Math.random() * 50);
+	fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${APIkey}&limit=${limit}&rating=r&offset=${gifOffset}`)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			data.data.forEach(gif => {
+				let aspectRatio = "";
+				gif.images["480w_still"].width / gif.images["480w_still"].height >= 1.5
+					? (aspectRatio = "item-double")
+					: (aspectRatio = "");
+				const newGif = newGifItem("window", gif, aspectRatio);
+				$suggestedGifsContainer.append(newGif);
+			});
+		})
+		.catch(error => {
+			return error;
+		});
+}
+
+function fetchSuggestions(limit) {
+	const $suggestedGifsContainer = document.querySelector("#suggested-container");
+	const suggestionArray = [
+		"fun",
+		"puppy",
+		"random",
+		"oh+shit",
+		"kitten",
+		"rick+morty",
+		"sillicon+valley",
+		"cute",
+		"lol",
+		"wtf",
+		"game+of+thrones"
+	];
+	const suggestion = Math.floor(Math.random() * (suggestionArray.length - 1));
+	fetch(
+		`http://api.giphy.com/v1/gifs/search?q=${suggestionArray[suggestion]}&api_key=${APIkey}&limit=${limit}&rating=r`
+	)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			data.data.forEach(gif => {
+				const newGif = newGifItem("window", gif);
+				$suggestedGifsContainer.append(newGif);
+			});
+		})
+		.catch(error => {
+			return error;
+		});
+}
+
+function newGifItem(type, gif, ratio = "") {
+	if (type === "window") {
+		const $container = document.createElement("div");
+		const $element = `<div class="window-item ${ratio}">
+				<div class="wi-header">
+					${gif.title}
+					<button class="remove-element"></button>
+				</div>
+				<div class="img-container">
+					<img class="img-element" src="${gif.images.original.url}" />
+					<a href="${gif.bitly_url}" target="_blank" type="button" class="btn-primary tag"><span>Ver más...</span></a>
+				</div>
+			</div>`;
+		$container.innerHTML = $element;
+		return $container.firstChild;
+	}
+}
