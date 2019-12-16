a140d3aa7701d1818634d161f0211321b2604502
Guido Torres <guido732@gmail.com>
2019-12-16 17:30:37 -0300

    Feature - Search by input / Suggestions (#9)
    * Refactors Fetch function
    
    * Adds search feature with display/hide search results block
    
    * Prevents default behaviour of form on submission and creates initial function
    
    * Completes search functionality on form submit
    
    * Corrects search suggestions zindex and adds grid flow dense
    
    * Adds new suggestions and corrects z-index on search suggestions
    
    * Adds search suggestion clicking functionality that triggers new search
    
    * Removes empty title suggestions from suggestion list

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const APIkey = "KvIjm5FP077DsfgGq2kLnXDTViwRJP7f";
 
+// Fetch new items on page load for trending and suggestions section
+window.onload = () => {
+	fetchSuggestions(4);
+	fetchTrending(20);
+};
+
 // Dropdown list visibility toggle
 document.querySelector("#dropdown-btn").onclick = function(e) {
 	const $dropdownList = document.querySelector("#dropdown-list");
@@ -15,110 +21,158 @@ document.onclick = function(e) {
 	}
 };
 
-// Disables submit button if search bar is empty
+// Handles search bar functionality
 document.querySelector("#search-bar").oninput = function(e) {
 	const $searchButton = document.querySelector("#search-button");
 	if (e.target.value !== "") {
 		$searchButton.disabled = false;
+		document.querySelector("#search-suggestions").style.display = "block";
+		fetchSearchTitles(7, document.querySelector("#search-bar").value);
 	} else {
 		$searchButton.disabled = true;
+		document.querySelector("#search-suggestions").style.display = "none";
 	}
 };
 
-// Fetch new items on page load for trending section
-window.onload = () => {
-	fetchSuggestions(4);
-	fetchTrending(20);
+// Gets search results from form submission
+document.searchform.onsubmit = e => {
+	e.preventDefault();
+	fetchSearchResults(20, document.querySelector("#search-bar").value);
 };
 
-function fetchTrending(limit) {
-	const $suggestedGifsContainer = document.querySelector("#trend-grid");
-	const gifOffset = Math.floor(Math.random() * 50);
-	fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${APIkey}&limit=${limit}&rating=r&offset=${gifOffset}`)
+// Generic fetch function
+function fetchURL(url) {
+	const fetchData = fetch(url)
 		.then(response => {
 			return response.json();
-		})
-		.then(data => {
-			data.data.forEach(gif => {
-				let aspectRatio = "";
-				gif.images["480w_still"].width / gif.images["480w_still"].height >= 1.5
-					? (aspectRatio = "item-double")
-					: (aspectRatio = "");
-				const newGif = newGifItem("trend", gif, aspectRatio);
-				$suggestedGifsContainer.append(newGif);
-			});
 		})
 		.catch(error => {
 			return error;
 		});
+	return fetchData;
 }
 
-function fetchSuggestions(limit) {
-	const $suggestedGifsContainer = document.querySelector("#suggested-container");
+async function fetchTrending(limit) {
+	const $trendingGifs = document.querySelector("#trend-grid");
+	const gifOffset = Math.floor(Math.random() * 50);
+
+	gifsTrending = await fetchURL(
+		`http://api.giphy.com/v1/gifs/trending?api_key=${APIkey}&limit=${limit}&offset=${gifOffset}`
+	);
+	gifsTrending.data.forEach(gif => {
+		let aspectRatio = "";
+		gif.images["480w_still"].width / gif.images["480w_still"].height >= 1.5 ? (aspectRatio = "item-double") : null;
+		$trendingGifs.append(newElement("trend", gif, aspectRatio));
+	});
+}
+
+async function fetchSuggestions(limit) {
+	const $suggestedGifs = document.querySelector("#suggested-container");
 	const suggestionArray = [
 		"baby+yoda",
-		"puppy",
-		"kitten",
+		"adventure+time",
 		"oh+shit",
 		"rick+morty",
 		"the+office",
 		"sillicon+valley",
-		"lol",
-		"wtf",
-		"shocked"
+		"the+mandalorian",
+		"pulp+fiction",
+		"fight+club",
+		"wtf"
 	];
 	const suggestion = Math.floor(Math.random() * (suggestionArray.length - 1));
-	fetch(
-		`http://api.giphy.com/v1/gifs/search?q=${suggestionArray[suggestion]}&api_key=${APIkey}&limit=${limit}&rating=r`
-	)
-		.then(response => {
-			return response.json();
-		})
-		.then(data => {
-			data.data.forEach(gif => {
-				const newGif = newGifItem("window", gif);
-				$suggestedGifsContainer.append(newGif);
-			});
-		})
-		.catch(error => {
-			return error;
-		});
+
+	gifsSuggestions = await fetchURL(
+		`http://api.giphy.com/v1/gifs/search?q=${suggestionArray[suggestion]}&api_key=${APIkey}&limit=${limit}`
+	);
+
+	gifsSuggestions.data.forEach(gif => {
+		$suggestedGifs.append(newElement("window", gif));
+	});
 }
 
-function newGifItem(type, gif, ratio = "") {
-	gif.title === "" ? (gif.title = "&emsp;") : null;
-	if (type === "window") {
-		const $container = document.createElement("div");
-		const $element = `<div class="window-item ${ratio}">
+async function fetchSearchTitles(limit, keywords) {
+	const $searchSuggestions = document.querySelector("#search-suggestions");
+	processedKeywords = keywords.split(" ").join("+");
+
+	searchResults = await fetchURL(
+		`http://api.giphy.com/v1/gifs/search?q=${processedKeywords}&api_key=${APIkey}&limit=${limit}`
+	);
+	$searchSuggestions.innerHTML = "";
+	searchResults.data.forEach(searchTitle => {
+		searchTitle.title ? $searchSuggestions.append(newElement("searchTitle", searchTitle)) : null;
+	});
+
+	const $searchSuggestionsBtn = document.querySelectorAll(".btn-search-suggestion");
+	$searchSuggestionsBtn.forEach(element => {
+		element.onclick = e => {
+			fetchSearchResults(20, element.innerText);
+		};
+	});
+}
+
+async function fetchSearchResults(limit, keywords) {
+	const $searchResults = document.querySelector("#search-results");
+	const $searchResultsContainer = document.querySelector("#search-result-container");
+	processedKeywords = keywords.split(" ").join("+");
+
+	searchResults = await fetchURL(
+		`http://api.giphy.com/v1/gifs/search?q=${processedKeywords}&api_key=${APIkey}&limit=${limit}`
+	);
+
+	$searchResultsContainer.innerHTML = "";
+	$searchResults.style.display = "block";
+	document.querySelector("#trends").style.display = "none";
+	document.querySelector("#suggestions").style.display = "none";
+	document.querySelector("#search-suggestions").style.display = "none";
+
+	searchResults.data.forEach(gif => {
+		let aspectRatio = "";
+		gif.images["480w_still"].width / gif.images["480w_still"].height >= 1.5 ? (aspectRatio = "item-double") : null;
+		$searchResultsContainer.append(newElement("trend", gif, aspectRatio));
+	});
+}
+
+function newElement(type, element, ratio = "") {
+	element.title === "" ? (element.title = "&emsp;") : null;
+	const $container = document.createElement("div");
+	let $element = "";
+	switch (type) {
+		case "window":
+			$element = `<div class="window-item ${ratio}">
 				<div class="wi-header">
-						${gif.title}
+						${element.title}
 					<button class="remove-element"></button>
 				</div>
 				<div class="img-container">
-					<img class="img-element" src="${gif.images.original.url}" />
-					<a href="${gif.bitly_url}" target="_blank" type="button" class="btn-primary tag"><span>Ver más...</span></a>
+					<img class="img-element" src="${element.images.original.url}" />
+					<a href="${element.bitly_url}" target="_blank" type="button" class="btn-primary tag"><span>Ver más...</span></a>
 				</div>
 			</div>`;
-		$container.innerHTML = $element;
-		return $container.firstChild;
-	} else if (type === "trend") {
-		// console.log(gif);
-		var titleToArray = gif.title.split(" ");
-		var titleArrayToTags = "";
-		titleToArray.forEach(word => {
-			titleArrayToTags += `#${word} `;
-		});
-		const $container = document.createElement("div");
-		const $element = `<div class="trend-item ${ratio}">
-			<a href="${gif.bitly_url}" target="_blank">
-				<img src="${gif.images.original.url}" alt="${gif.title}" class="img-element" />
-				<div class="trend-header">
-					${titleArrayToTags}
-				</div>
-			</a>
-		</div>
-	</div>`;
-		$container.innerHTML = $element;
-		return $container.firstChild;
+			$container.innerHTML = $element;
+			return $container.firstChild;
+		case "trend":
+			const titleToArray = element.title.split(" ");
+			let titleArrayToTags = "";
+			titleToArray.forEach(word => {
+				titleArrayToTags += `#${word} `;
+			});
+			$element = `<div class="trend-item ${ratio}">
+				<a href="${element.bitly_url}" target="_blank">
+					<img src="${element.images.original.url}" alt="${element.title}" class="img-element" />
+					<div class="trend-header">
+						${titleArrayToTags}
+					</div>
+				</a>
+			</div>
+		</div>`;
+			$container.innerHTML = $element;
+			return $container.firstChild;
+		case "searchTitle":
+			$element = `<button class="search-element btn-search-suggestion">
+		<span>${element.title}</span>
+		</button>`;
+			$container.innerHTML = $element;
+			return $container.firstChild;
 	}
 }
