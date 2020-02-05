5ba3ae455c1c74fed9b6e191421ca50896173e59
guido732 <guido732@gmail.com>
2020-02-05 19:59:52 -0300

    feat(css refactor): Refactors remaining elements to BEM + ITCSS

--- js/main.js
+++ js/main.js
@@ -47,6 +47,8 @@ const navBar = (() => {
 
 	// Bind events
 	events.on("pageLoad", loadColorTheme);
+	events.on("gotoHome", () => hideElements($returnArrow));
+	events.on("createGifEnded", () => hideElements($returnArrow));
 	$homeButton.addEventListener("click", () => {
 		// Takes user to default window view
 		events.emit("gotoHome");
@@ -90,6 +92,7 @@ const navBar = (() => {
 	}
 	function showCreateGifSection() {
 		events.emit("createGif");
+		showElements($returnArrow);
 		hideElements($navItems);
 	}
 	function mount() {
@@ -1107,73 +1110,3 @@ function lazyLoadImages() {
 	closeOpenedElements -> event launched when clicking on body of page or pressing scape funciton to close suggestions and modals
 
 */
-
-/* const paginator = (totalItems = 0, currentPage = 1, pageSize = 30, maxPages = 10) => {
-	// calculate total pages
-	let totalPages = Math.ceil(totalItems / pageSize);
-
-	// ensure current page isn't out of range
-	if (currentPage < 1) {
-		currentPage = 1;
-	} else if (currentPage > totalPages) {
-		currentPage = totalPages;
-	}
-
-	let startPage, endPage;
-	if (totalPages <= maxPages) {
-		// total pages less than max so show all pages
-		startPage = 1;
-		endPage = totalPages;
-	} else {
-		// total pages more than max so calculate start and end pages
-		let maxPagesBeforeCurrentPage = Math.floor(maxPages / 2);
-		let maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1;
-		if (currentPage <= maxPagesBeforeCurrentPage) {
-			// current page near the start
-			startPage = 1;
-			endPage = maxPages;
-		} else if (currentPage + maxPagesAfterCurrentPage >= totalPages) {
-			// current page near the end
-			startPage = totalPages - maxPages + 1;
-			endPage = totalPages;
-		} else {
-			// current page somewhere in the middle
-			startPage = currentPage - maxPagesBeforeCurrentPage;
-			endPage = currentPage + maxPagesAfterCurrentPage;
-		}
-	}
-
-	// calculate start and end item indexes
-	let startIndex = (currentPage - 1) * pageSize;
-	let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-
-	// create an array of pages to ng-repeat in the pager control
-	let pages = Array.from(Array(endPage + 1 - startPage).keys()).map(i => startPage + i);
-
-	// return object with all pager properties required by the view
-	return {
-		totalItems: totalItems,
-		currentPage: currentPage,
-		pageSize: pageSize,
-		totalPages: totalPages,
-		startPage: startPage,
-		endPage: endPage,
-		startIndex: startIndex,
-		endIndex: endIndex,
-		pages: pages
-	};
-};
-const itemsToPaginate = Array.from(Array(150).keys()).map(i => ({ id: i + 1, name: "Item " + (i + 1) }));
-const state = {
-	itemsToPaginate: itemsToPaginate,
-	pageOfItems: []
-};
-let fakePagination = paginator(state.itemsToPaginate.length);
-
-state.pageOfItems = [];
-console.log(state.pageOfItems);
-
-for (let i = fakePagination.startIndex; i <= fakePagination.endIndex; i++) {
-	state.pageOfItems.push(itemsToPaginate[i]);
-}
-console.log(state.pageOfItems); */
